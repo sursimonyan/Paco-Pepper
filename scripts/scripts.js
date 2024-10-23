8579fd253d596e4ea740056ea3ed830efe77eaf4
Suren Simonyan <s.simonyan@combotech.net>
2024-10-24 00:41:58 +0400

    added faq section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,8 @@
 const mediaQuery = window.matchMedia('(min-width: 991px)')
 const header__mobile_menu = document.querySelector('.header__mobile_menu');
 const header__menu_close = document.querySelector('.header__menu_close');
-const litter__text_wrap = document.querySelector('.litter__text_wrap');
-const litter__open_close_icon = document.querySelectorAll('.litter__open_close_icon');
+const faq__text_wrap = document.querySelector('.faq__text_wrap');
+const faq__package_info = document.querySelectorAll('.faq__package_info');
 const header__nav = document.querySelector('.header__nav');
 const body = document.querySelector('body');
 
@@ -24,14 +24,15 @@ function closeBurgerMenu() {
 }
 
 function openCloseInfo() {
-  const toggleElement = this.parentNode.parentNode.querySelector('.litter__text_wrap')
+  const toggleElement = this.querySelector('.faq__text_wrap')
+  const openCloseIcon = this.querySelector('.faq__open_close_icon')
   toggleElement.classList.toggle('open')  
-  this.classList.toggle('open')
+  openCloseIcon.classList.toggle('open')
 }
 
 header__mobile_menu.addEventListener('click', openBurgerMenu, false)
 header__menu_close.addEventListener('click', closeBurgerMenu, false)
-litter__open_close_icon.forEach(el=>el.addEventListener('click', openCloseInfo, false))
+faq__package_info.forEach(el=>el.addEventListener('click', openCloseInfo, false))
 
 mediaQuery.addEventListener('change', handleTabletChange, false)
 
